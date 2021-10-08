c7730fdc99694cb2e96b977c23beffbdb8c4cf44
FiddleBlink <87928742+FiddleBlink@users.noreply.github.com>
2021-10-08 09:26:47 +0530

    Create Space Invaders.py

---
+++ Space Invaders.py
@@ -0,0 +1,22 @@
+import pygame
+from pygame.constants import SCRAP_SELECTION
+
+# Initialize the pygame library
+pygame.init()
+
+# create the display
+screen = pygame.display.set_mode((800, 600))
+
+#title and icon
+pygame.display.set_caption("Space Invasion")
+icon = pygame.image.load('pygametut/ufo.png')
+pygame.display.set_icon(icon)
+
+# game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
